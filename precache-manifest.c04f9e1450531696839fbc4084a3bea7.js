329ecf575b0b21bcf00c4e267a5df54f38cc03b2
Grygoriy Shytikov <gregorysgm@gmail.com>
2021-05-31 15:58:24 +0300

    Updates

---
+++ precache-manifest.c04f9e1450531696839fbc4084a3bea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa434469a03590576f2dcdd427088741",
+    "url": "./index.html"
+  },
+  {
+    "revision": "079b895f4169d4b589c5",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "ec0f0853e78137ba914a",
+    "url": "./static/css/main.04d85d33.chunk.css"
+  },
+  {
+    "revision": "079b895f4169d4b589c5",
+    "url": "./static/js/2.51a060dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.51a060dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0f0853e78137ba914a",
+    "url": "./static/js/main.da8812bf.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
